4e43dfa8eef2e2d62b0fac1946dc7141f7007ecd
Kaung Htet <henrykaunghtet2006@gmail.com>
2026-09-24 10:48:24 +0630

    huuuu

--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ Commit Push tal
 [![LICENSE](https://img.shields.io/github/license/henrykaunghtet2006-cyber/master.svg?style=flat-square)](https://github.com/henrykaunghtet2006-cyber/master/blob/master/LICENSE)
 [![Releases](https://img.shields.io/github/release/henrykaunghtet2006-cyber/master/all.svg?style=flat-square)](https://github.com/henrykaunghtet2006-cyber/master/releases)
 ![GitHub Workflow Status (branch)](https://img.shields.io/github/actions/workflow/status/henrykaunghtet2006-cyber/master/main.yml?branch=devlop)
+
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,7 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
@@ -11,7 +12,8 @@ public class App
     {
         // Connect to MongoDB on local system - we're using port 27000
         // Connect to MongoDB
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+        MongoClient mongoClient = MongoClients.create("mongodb://mongo-dbserver:27017");
+
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
